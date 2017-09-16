6eeab3bd25314bbf7f62470dccf1957d240324e7
hhiroki <hhiroki@hhirokiMBA.local>
2017-09-16 12:58:19 +0900

    Add checkbox and insert records function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,22 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def edit
   end
 
   def create
+    Group.create(create_params)
   end
 
   def update
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name, user_ids: [])
   end
 
 end
