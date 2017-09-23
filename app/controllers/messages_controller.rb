a868c8c022bbc7049519c1dda15d231e21c4bf93
hhiroki <hhiroki@hhirokiMBA.local>
2017-09-23 16:45:06 +0900

    Add message send function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,21 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(get_msg_params)
+    if @message.save
+      redirect_to group_messages_path, notice: 'メッセージを作成しました'
+    else
+      redirect_to group_messages_path, alert: 'メッセージを入力してください'
+    end
+  end
+
+  private
+  def get_msg_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  validates :body, presence: true
+
+  belongs_to :user
+  belongs_to :group
 end
