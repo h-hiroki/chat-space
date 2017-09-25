1b2ba80b4c1393ec3c0694c7e319421892021a2d
hhiroki <hhiroki@hhirokiMBA.local>
2017-09-25 21:57:57 +0900

    Edit sidebar's group last message and add scroll.

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,12 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+
+  def last_msg
+    if messages.blank?
+      "まだメッセージはありません"
+    else
+      messages.last.body
+    end
+  end
 end
