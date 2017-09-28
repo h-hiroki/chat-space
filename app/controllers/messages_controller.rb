0dcf18e3f9c8faa9382ef7f723258690418fbc15
hhiroki <hhiroki@hhirokiMBA.local>
2017-09-28 22:55:10 +0900

    Show message record

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,6 +22,6 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
-    @message = @group.messages
+    @messages = @group.messages
   end
 end
