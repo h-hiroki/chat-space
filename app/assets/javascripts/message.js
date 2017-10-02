7b87e750308dcd46939387438021812933cb321f
hhiroki <hhiroki@hhirokiMBA.local>
2017-10-02 23:30:23 +0900

    message post by Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+  function buildHTML(message){
+    var html_image = ""
+    if(message.image){
+      html_image = `<img src="${message.image}" class="contents__body__messeage-image">`
+    }
+    var html = `<div class="contents__body__messeage">
+                  <div class="contents__body__messeage-name">
+                    ${message.name}
+                  </div>
+                  <div class="contents__body__messeage-time">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="contents__body__messeage-body">
+                    ${message.body}
+                </div>
+                  ${html_image}`
+    return html;
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -12,6 +31,18 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.contents__body__messeage-list').append(html);
+      $('#contents__footer__form').val('');
+      $('.contents__footer__btn__input-field').val('');
+      $('.contents__body').animate( {scrollTop: $('.contents__body__messeage-list')[0].scrollHeight} );
+    })
+    .fail(function(){
+      alert('error');
+    })
+
   })
 
-})
+});
